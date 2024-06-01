6e9556eb6089343abbe0e87d61e08507e07b1e3f
ZolaBnr <163735672+ZolaBnr@users.noreply.github.com>
2024-06-01 13:09:37 -0800

    Update full_screen_loader.dart

--- lib/utils/popups/full_screen_loader.dart
+++ lib/utils/popups/full_screen_loader.dart
@@ -14,32 +14,30 @@ class TFullScreenLoader {
   /// - animation: the Lottie animation to be shown
   static void openLoadingDialog(String text, String animation) {
     showDialog(
-      context: Get.overlayContext!,
-      // Use Get.overlayContext for overlay dialogs
-      barrierDismissible: false,
-      // The dialog can't be dismissed by tapping outside it
-      builder: (_) {
-        return PopScope(
-          canPop: false,
-          child: Container(
-            color: HelperFunctions.isDarkMode(Get.context!)
-                ? TColors.dark
-                : TColors.white,
-            width: double.infinity,
-            height: double.infinity,
-            child: Column(
-              children: [
-                const SizedBox(height: 250),
-                TAnimationLoaderWidget(
-                  text: text,
-                  animation: animation,
-                  actionText: '',
-                ),
-              ],
-            ),
+      context:
+          Get.overlayContext!, // Use Get.overlayContext for overlay dialogs
+      barrierDismissible:
+          false, // The dialog can't be dismissed by tapping outside it
+      builder: (_) => PopScope(
+        canPop: false, //Disable popping with back button
+        child: Container(
+          color: HelperFunctions.isDarkMode(Get.context!)
+              ? TColors.dark
+              : TColors.white,
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              const SizedBox(height: 250), //Adjust the spacing as needed
+              TAnimationLoaderWidget(
+                text: text,
+                animation: animation,
+                actionText: '',
+              ),
+            ],
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 
